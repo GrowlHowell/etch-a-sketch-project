19463db15434bf4c210014eefa54e0b94bb9998a
Justin Howell <justinmhowell@protonmail.com>
2022-07-31 16:42:41 -0400

    Create grid resize button

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
 const container = document.querySelector('.container');
 let div;
+let btn = document.getElementById('grid-resize');
 
 const randomColor = function() {
     let color = '#' + Math.floor(Math.random()*16777215).toString(16);
     return color;
 }
 
-for (i = 0; i < 16; i++) {
-    div = document.createElement('div');
-    div.classList.add('grid');
-    container.appendChild(div);
+let gridSize = function(size) {
+    for (i = 0; i < size; i++) {
+        div = document.createElement('div');
+        div.classList.add('grid');
+        container.appendChild(div);
+    }
 }
+
+gridSize(16);
+
+const resizeGrid = function() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    let selection = prompt("How many grid squares would you like?");
+    while (selection > 100) {
+        selection = prompt("Please select a number up to 100.");
+        if (selection < 100) {
+            return selection;
+        }
+    }
+    gridSize(selection);
+};
+
+btn.addEventListener('click', function() {
+    resizeGrid();
+})
 
 container.addEventListener('mouseover', function(e) {
     if(e.target.classList.contains('grid')) {
